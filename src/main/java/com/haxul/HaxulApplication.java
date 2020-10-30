db39eb9466bd648478028dc239db94f62c3d2260
haxul <s.s.starodubov@yandex.ru>
2020-10-30 12:40:39 +0400

    init

---
+++ src/main/java/com/haxul/HaxulApplication.java
@@ -0,0 +1,13 @@
+package com.haxul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HaxulApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HaxulApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/haxul/HaxulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.haxul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HaxulApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
