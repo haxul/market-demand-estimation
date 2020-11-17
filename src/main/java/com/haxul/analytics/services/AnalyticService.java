a898fbfeff7c3ef740f07a66e11a4e5a9554c074
haxul <s.s.starodubov@yandex.ru>
2020-11-17 21:31:46 +0400

    add

--- src/main/java/com/haxul/analytics/services/AnalyticService.java
+++ src/main/java/com/haxul/analytics/services/AnalyticService.java
@@ -4,6 +4,7 @@ package com.haxul.analytics.services;
 import com.haxul.analytics.DateType;
 import com.haxul.analytics.dto.Difference;
 import com.haxul.analytics.exceptions.UnSupportedDateTypeException;
+import com.haxul.headhunter.models.area.City;
 import com.haxul.headhunter.repositories.HeadHunterRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
@@ -16,7 +17,7 @@ public class AnalyticService {
     private final HeadHunterRepository headHunterRepository;
 //TODO create analytics
     @SneakyThrows
-    public Difference getDifference(DateType dateType, short num) {
+    public Difference getDifference(DateType dateType, int previousDateItems, City city, String position) {
         switch (dateType) {
             case DAY: return null;
             case MONTH: return null;
--- src/main/java/com/haxul/headhunter/repositories/HeadHunterRepository.java
+++ src/main/java/com/haxul/headhunter/repositories/HeadHunterRepository.java
@@ -3,6 +3,8 @@ package com.haxul.headhunter.repositories;
 import com.haxul.headhunter.entities.MarketDemand;
 import com.haxul.headhunter.models.area.City;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Date;
@@ -12,4 +14,9 @@ import java.util.List;
 public interface HeadHunterRepository extends JpaRepository<MarketDemand, Long> {
 
     List<MarketDemand> findByPositionAndCityAndAtMoment(String position, City city, Date date);
+
+    @Query(value = "SELECT * FROM market_demands WHERE position= ?1" +
+            " AND city= ?2" +
+            " AND at_moment > clock_timestamp() - (interval '1' day) * ?3", nativeQuery = true)
+    List<MarketDemand> findAllDaysAgo(String position, String city, int days);
 }
